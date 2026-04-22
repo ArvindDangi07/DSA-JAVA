77d81966f13a92c29fbd56b632103b575772a513
ArvindDangi07 <arvindkumar131220@gmail.com>
2026-04-22 21:02:48 +0530

    changes

--- src/basic/Palindrom.java
+++ src/basic/Palindrom.java
@@ -24,3 +24,43 @@ public class Palindrom {
 
     }
 }
+
+
+/*
+
+package basic;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+
+        StringBuilder rev = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev.append(str.charAt(i));
+        }
+
+        if (str.equalsIgnoreCase(rev.toString())) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+ */
