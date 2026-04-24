cda119aa628f30599fc3ad1c33ced73919604a69
ArvindDangi07 <arvindkumar131220@gmail.com>
2026-04-24 20:21:45 +0530

    {Arrays] Basic concept for length method

---
+++ src/array/LengthMethod.java
@@ -0,0 +1,15 @@
+package array;
+
+public class LengthMethod {
+    public static void main(String[] args) {
+        String str = "Durga Soft";
+        System.out.println(str.length());
+
+    }
+}
+/*
+
+# length() is a final method which is used for string object;
+to count to available character inside the string object
+
+ */
