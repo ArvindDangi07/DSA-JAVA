6ccdb0e81f2b2ed3b94b5fbba8753a599272d6d4
ArvindDangi07 <arvindkumar131220@gmail.com>
2026-04-24 20:11:41 +0530

    {Arrays] Basic concept for length variable

---
+++ src/array/Length.java
@@ -0,0 +1,34 @@
+package array;
+
+public class Length {
+
+    public static void main(String[] args) {
+        int [] arr = {2,4,5,2,6,1};
+        System.out.println(arr.length);
+
+
+        int [][] nums = new int [2][3];
+        System.out.println(nums.length); // no.of row
+        System.out.println(nums[0].length);
+        System.out.println(nums[1].length);
+
+
+    }
+}
+/*
+
+# length is a final variable which applicable only on arrays.
+to find size of array.
+
+# we cant find total size of array in multidimensional  array.
+but we can find in chunks;
+
+
+
+
+
+
+
+
+
+ */
