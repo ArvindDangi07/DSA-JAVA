870659d92e8e535feb80d6df5b8429294cefe63a
ArvindDangi07 <arvindkumar131220@gmail.com>
2026-05-06 22:12:02 +0530

    Immutable_Mutable

---
+++ src/string/Immutable_Mutable.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.util.Arrays;
+
+public class Immutable_Mutable {
+    public static void main(String[] args) {
+        String str = "Arvind";
+        str.replace('A','W'); // Arvind : cant change it; (immutable)
+        System.out.println(str);
+
+        char[] ch = str.toCharArray();
+        ch[0]='W';
+        System.out.println(ch); // Wrvind : can change (mutable)
+
+    }
+
+}
