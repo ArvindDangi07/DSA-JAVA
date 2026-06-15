8702c41c88d4f8e18882e757940eff08eb32e399
ArvindDangi07 <arvindkumar131220@gmail.com>
2026-06-15 22:07:06 +0530

    m

---
+++ src/recursion/BaseConditionRecursion.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class BaseConditionRecursion {
+
+
+    static int count = 0;
+
+
+    public static void main(String[] args) {
+        m2();
+
+    }
+
+
+    public static void m2(){
+
+        if(count==3){
+            return;
+        }
+        System.out.println(count);
+        count++;
+
+        m2();
+
+
+
+    }
+}
